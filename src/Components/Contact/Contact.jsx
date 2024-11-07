3697bfca5cf39edcc46695dccf9fc7c5d852d102
justinait <justiiturraspe@gmail.com>
2024-11-07 10:33:20 -0300

    contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,7 +3,29 @@ import './Contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='contactContainer'></div>
+      
+
+      <div>
+        <h5>Nuestros turistas</h5>
+        <p>Formando parte de Open Club, a través del programa
+        “Nuestros Turistas”, entre ellos destacamos:</p>
+
+        <h4>Info</h4>
+        <p>Acceso exclusivo a nuestra Carpeta de Huéspedes, donde te mostramos todas las actividades imperdibles de Paraná.</p>
+        <ul>
+          <li>30% OFF en Excursiones de Costanera 241</li>
+          <li>20% OFF en Excursiones Náuticas con Paraná Tu Río.</li>
+          <li>20% OFF en Visitas a Bodegas y Viñedos con Vinos de Ríos.</li>
+          <li>10% OFF en la compra de Vinos Entrerrianos, en nuestra Vinoteca.</li>
+          <li>10% OFF en Excursiones Exclusivas a la ciudad de Santa Fe, de la mano de Mirá Santa Fé.</li>
+        </ul>
+
+        <p>¡Únicamente los huéspedes de Open Club
+        forman parte de nuestro programa de beneficios!</p>
+      </div>
+    </div>
   )
 }
 
